ad447db4e35cb729213c8de8f6f9062d9802e97a
thanugeorge13 <thanugeorge.cs23@bmsce.ac.in>
2025-05-24 10:11:08 +0530

    Update prim.c

--- prim.c
+++ prim.c
@@ -1,40 +1,60 @@
-#include<stdio.h>
-#include<stdbool.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <limits.h>
 
-void main(){
+int main() {
     int v;
-    printf("enter no of vertices\t");
-    scanf("%d",&v);
+    printf("Enter number of vertices: ");
+    scanf("%d", &v);
+    
     int G[v][v];
-    printf("enter adjacency matrix\n");
-    for(int i=0;i<v;i++){
-        for(int j=0;j<v;j++)
-            scanf("%d",&G[i][j]);
+    printf("Enter adjacency matrix:\n");
+    for (int i = 0; i < v; i++) {
+        for (int j = 0; j < v; j++) {
+            scanf("%d", &G[i][j]);
+        }
     }
+    
     printf("EDGE  -  WEIGHT\n");
-    int no_edges=0;
-    int selected[v];
-    memset(selected,false,sizeof(selected));
-    selected[0]="true";
-    int row,col;
-    while(no_edges < v-1){
-        int min=99999;
-        row=0;col=0;
-        for(int i=0;i<v;i++){
-            if(selected[i]){
-            for(int j=0;j<v;j++){
-                if(!selected[j] && G[i][j]){
-                    if(min> G[i][j]){
-                        min=G[i][j];
-                        row=i;
-                        col=j;
+    int no_edges = 0;
+    bool selected[v];
+    memset(selected, false, sizeof(selected));
+    selected[0] = true; // Start from the first vertex
+
+    int row, col;
+    int total_weight = 0; // Variable to store the total weight of the MST
+
+    while (no_edges < v - 1) {
+        int min = INT_MAX; // Use INT_MAX for initialization
+        row = 0; col = 0;
+
+        for (int i = 0; i < v; i++) {
+            if (selected[i]) {
+                for (int j = 0; j < v; j++) {
+                    if (!selected[j] && G[i][j]) {
+                        if (min > G[i][j]) {
+                            min = G[i][j];
+                            row = i;
+                            col = j;
+                        }
                     }
                 }
             }
         }
+
+        if (min == INT_MAX) {
+            printf("No more edges to add.\n");
+            break; // No more edges to add
+        }
+
+        printf("%d - %d : %d\n", row, col, G[row][col]);
+        total_weight += G[row][col]; // Add the weight of the edge to total weight
+        selected[col] = true;
+        no_edges++;
     }
-    printf("%d - %d : %d\n", row, col, G[row][col]);
-    selected[col] = true;
-    no_edges++;
-    }
+
+    printf("Total weight of the Minimum Spanning Tree: %d\n", total_weight); // Print total weight
+    return 0;
 }
+
